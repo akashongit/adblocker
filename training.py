8e581ba0034bd6dda391e3f03a5fd5a250ca1530
akashongit <akashgeevarghese.mec@gmail.com>
2018-02-26 01:56:30 +0530

    training

--- training.py
+++ training.py
@@ -9,12 +9,31 @@ urls = csvfile.values.tolist()
 
 fullset =[]
 
-for content in urls:
+for content in urls[:3000]:
     url_class = features.copy()
     url_class[content[0]]=True
     url_class[content[1]]=True
     url_class[content[2]]=True
     url_class[content[3]]=True
-    fullset.append(url_class)
+    res = content[4]
+    # if content[4] == -1:
+        # res = "neg"
+    # else :
+        # res = "pos"
+    fullset.append((url_class,res))
+    # fullset.append(url_class)
+    # print(fullset)
+# print(url_class)
+# training_set = urlclass[:1900]
+#
+# # set that we'll test against.
+training_set = fullset
+testing_set = fullset[1900:]
+print("Entered NaiveBayesClassifier")
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("learnt!!!")
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
 
-print(fullset)
+save_classifier = open("naivebayes.pickle","wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
