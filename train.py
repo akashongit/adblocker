73060c7494478885482ce00d62eced41cd7b9e84
akashongit <akashgeevarghese.mec@gmail.com>
2018-02-25 15:31:12 +0530

    dataset changes

--- train.py
+++ train.py
@@ -1,22 +1,29 @@
-from pandas import read_csv
+import pandas as pd
 import nltk
 import re
 # from collections import Counter
-
-t = read_csv("./datasets/dmoz0409_Arts_test.csv")
+# lis =()
+lis = []
+t = pd.read_csv("./datasets/dmoz0409_Arts_test.csv")
 # print (t.ix[:,1:])
 str_url = (t.ix[:,1:2])
 str_class = (t.ix[:,2:])
 
 urls = str_url.to_string(index = False)
-clas = str_class.to_string(index = False)
+# clas = str_class.to_string(index = False)
+clas = str_class.values.tolist()
 cleaned_urls = []
-# urls.lstrip()
+# print(clas)
+myiter = iter(clas)
 list_urls = urls.split('\n')
 for row in list_urls:
+    try:
+        url_clas = next(myiter)
+    except Exception as e:
+        pass
     # print(type(row))
-    nrow = re.sub('[\s!@#$+_.-/:=&?~\d]',' ', row)
-    # print(nrow)
+    nrow = re.sub('[\s!@#$+_.\-/:=&?~\d]',' ', row)
+    # printpass)
     # tokens = nltk.word_tokenize(nrow)
     # print(tokens)
     # fourgrams=nltk.collocations.QuadgramCollocationFinder.from_words(tokens)
@@ -24,20 +31,32 @@ for row in list_urls:
         # print (fourgram)
     n = 4
     fourgrams = nltk.ngrams(nrow.split(), n)
+    # forgrams = filter(None,list(fourgrams))
+    # forgrams = [list(x) for x in fourgrams]
+    # forgrams = list(fourgrams)
+    # if forgrams != []:
+    # if fourgrams != []:
+        # print(forgrams)
+        # lis.append(next(myiter))
+        # lis.append(forgrams)
+        # lis = lis + tuple(next(myiter))
+        # lis = lis + tuple(fourgrams)
     for grams in fourgrams:
-      print (grams)
+      # nlist = list(grams)
+      url_str = ','.join(grams)
+      print( url_str,',',url_clas[0])
     # cleaned_urls.append(nrow)
     # row = str(row)
     # print(nrow)
     # l = row.split('.').split('-').split('/').split('_').split('+')
     # string = "".join(l)
     # print (string)
-
 # print(cleaned_urls)
-
+# print(lis)
 
 # sentence = 'this is a foo bar sentences and i want to ngramize it'
 # n = 4
 # fourgrams = ngrams(nrow.split(), n)
 # for grams in fourgrams:
 #   print (grams)
+# print (str(fourgrams))
